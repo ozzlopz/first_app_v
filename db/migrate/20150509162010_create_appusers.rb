62001598356d4b8518f9159d12af3f123661ff42
ozzlopz <ozzlopz@gmail.com>
2015-05-09 17:38:27 +0000

    CSS in notif html

--- db/migrate/20150509162010_create_appusers.rb
+++ db/migrate/20150509162010_create_appusers.rb
@@ -1,6 +1,12 @@
 class CreateAppusers < ActiveRecord::Migration
   def change
     create_table :appusers do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :model
+      t.string :serial_number
+      t.timestamps
     end
   end
 end
