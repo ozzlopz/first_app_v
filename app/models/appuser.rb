8cc12a91f386316cabcc3dc4cf5b8c7333177244
ozzlopz <ozzlopz@gmail.com>
2015-05-10 03:17:27 +0000

    test

--- app/models/appuser.rb
+++ app/models/appuser.rb
@@ -1,12 +1,3 @@
 class Appuser < ActiveRecord::Base
-
-  validates :name, presence: true, length:{minimum:3,maximum:40}
-  validates :first_last_name, presence: true, length:{minimum:3,maximum:40}
-  validates :second_last_name, presence: true, length:{minimum:3,maximum:40}
-  validates :lada, presence: true, length:{minimum:3,maximum:40}
-  validates :phone, presence: true, length:{minimum:3,maximum:40}
-  validates :email, presence: true, length:{minimum:3,maximum:40}
-  validates :model, presence: true, length:{minimum:3,maximum:40}
-  validates :serial_number, presence: true, length:{minimum:3,maximum:40}
   
 end
