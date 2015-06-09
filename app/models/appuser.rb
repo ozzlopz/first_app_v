41b5bb675ca42c36dee1d1739eb765da39b3dec3
ozzlopz <ozzlopz@gmail.com>
2015-06-09 19:49:54 +0000

    Appuser wothout validations

--- app/models/appuser.rb
+++ app/models/appuser.rb
@@ -2,14 +2,7 @@ class Appuser < ActiveRecord::Base
   
   acts_as_xlsx
 
-  validates :name, presence: true, length:{minimum:3,maximum:40}
-  validates :first_last_name, presence: true, length:{minimum:3,maximum:40}
-  validates :second_last_name, presence: true, length:{minimum:3,maximum:40}
-  validates :lada, presence: true, length:{minimum:3,maximum:40}
-  validates :phone, presence: true, length:{minimum:3,maximum:40}
-  validates :email, presence: true, length:{minimum:3,maximum:40}
-  validates :model, presence: true, length:{minimum:3,maximum:40}
-  validates :year, presence: true, length:{minimum:3,maximum:4}
+  
   
   belongs_to :car
   
